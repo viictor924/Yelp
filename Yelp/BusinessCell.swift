a3eb949b2c145ed1b071bb887f5a4cd4d47d1a4f
Victor Rodriguez <viictor.rodriguez@gmail.com>
2017-04-09 12:32:45 -0700

    Completed search results page and filter page

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,48 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by victor rodriguez on 4/8/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var ratingImageView: UIImageView!
+   
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var reviewCountLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    var business: Business! {
+        didSet{
+            nameLabel.text = business.name
+            distanceLabel.text = business.distance
+            reviewCountLabel.text = "\(business.reviewCount!) Reviews"
+            addressLabel.text = business.address
+            categoryLabel.text = business.categories
+            
+            thumbImageView.setImageWith(business.imageURL!)
+            ratingImageView.setImageWith(business.ratingImageURL!)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        thumbImageView.layer.cornerRadius = 3
+        thumbImageView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Yelp/SwitchCell.swift
@@ -0,0 +1,40 @@
+//
+//  SwitchCell.swift
+//  Yelp
+//
+//  Created by victor rodriguez on 4/8/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol SwitchCellDelegate{
+    @objc optional func switchCell(switchCell: SwitchCell, didChangeValue value: Bool)
+}
+
+class SwitchCell: UITableViewCell {
+
+    @IBOutlet weak var switchLabel: UILabel!
+    @IBOutlet weak var onSwitch: UISwitch!
+    
+    weak var delegate: SwitchCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        onSwitch.addTarget(self, action: #selector(SwitchCell.switchValueChanged), for: UIControlEvents.valueChanged)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func switchValueChanged(){
+        print("switch value changed")
+            delegate?.switchCell?(switchCell: self, didChangeValue: onSwitch.isOn)
+        
+    }
+
+}
